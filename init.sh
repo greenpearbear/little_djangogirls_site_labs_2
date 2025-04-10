4fdc0b40201c901be0a28669753ac0b46923aa32
greenpearbear <romadmitr12@gmail.com>
2025-04-10 15:41:04 +0000

    create superuser if no superuser in database(5)

--- init.sh
+++ init.sh
@@ -9,8 +9,14 @@ password=pass
 nohup python3 manage.py runserver 0.0.0.0:8000 &
 
 sleep 60 &&
+
 python3 manage.py migrate &&
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+
+output=$(python3 manage.py shell -c "from django.contrib.auth.models import User; all_users = User.objects.values(); print(all_users[0]['is_superuser'])")
+
+if [ "True" != "$output" ]; then
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+fi
 
 wait
 
