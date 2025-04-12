0b93fbc1b52eb6cbbde6ffc88c1d6324eb0faf12
greenpearbear <romadmitr12@gmail.com>
2025-04-12 11:57:59 +0000

    Вынес миграции в отдельный сервис(10), но они не автоматические? и сначала миграция потом поднятие джанги(пункт 3 считается выполненым?)

--- init.sh
+++ init.sh
@@ -1,22 +1,9 @@
 #! /bin/sh
 
-# User credentials
-user=admin
-email=admin@example.com
-password=pass
-#file=db/db.sqlite3
-
 nohup python3 manage.py runserver 0.0.0.0:8000 &
-
-sleep 60 &&
-
-python3 manage.py migrate &&
 
 output=$(python3 manage.py shell -c "from django.contrib.auth.models import User; all_users = User.objects.values(); print(all_users[0]['is_superuser'])")
 
 if [ "True" != "$output" ]; then
-	echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('$USER', '$EMAIL', '$PASSWORD')" | python3 manage.py shell
 fi
-
-wait
-
